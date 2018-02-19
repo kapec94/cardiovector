20340355810f103dc755586174e73f8c0fbbf506
Mateusz Stanisław Kapłon <mateusz.kaplon@gmail.com>
2018-02-19 13:16:07 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='cardiovector',
+      version='0.1',
+      description='Vectocardiogram utilities library',
+      author='Mateusz Kapłon',
+      author_email='mateusz.kaplon@gmail.com',
+      url='https://github.com/kapec94/cardiovector',
+      packages=['cardiovector'])
